cbbf6d04fa9d13774213f1d2ca5b2ae6170b884c
GesaXB <541231083@student.smktelkom-pwt.sch.id>
2024-12-23 00:33:14 +0700

    vercel

--- myportofolio/settings.py
+++ myportofolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-qkd**r53(m+9&!pca^czxs0ab02@qrm)q!=21of%8bzyqvq!v9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
